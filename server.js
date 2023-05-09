cdd9818efef12af886ab1567cf7b7f0a32175556
turnagza <turnagza@ad.unc.edu>
2023-05-08 23:03:50 -0400

    server.js fix 3

--- server.js
+++ server.js
@@ -78,28 +78,36 @@ import {rps, rpsls} from "./lib/rpsls.js";
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get("/app", (req, res) => {
-    res.status(200).send("200 OK");});
-app.get("/app/rps", (req, res) => {
-    res.status(200).send(JSON.stringify(rps()));});
-app.get("/app/rpsls", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls()));});
-app.get("/app/rps/play", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)));});
-app.get("/app/rpsls/play", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));});
-app.post("/app/rps/play", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));});
-app.post("/app/rpsls/play", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));});
-app.get("/app/rps/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));});
-app.get("/app/rpsls/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));});
-app.get("*", (req, res) => {
-    res.status(404).send("404 NOT FOUND");});
-app.listen(port, () => {
-    console.log(`the app is lisning on port ${port}`);});
+app.get('/app', (req, res) => {
+    res.status(200).send('200 OK').end();
+});
+app.get('/app/rps', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+app.get('/app/rpsls', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+app.get('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot))).end();
+})
+app.get('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot))).end();
+})
+app.post('/app/rps/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot))).end();
+})
+app.post('/app/rpsls/play', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot))).end();
+})
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot))).end();
+})
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot))).end();
+})
+app.all('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND').end();
+})
 
 //A04 CODE END
 
