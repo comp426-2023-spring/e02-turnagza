217306a437611f3078ce3a7f79664348eed35566
turnagza <turnagza@ad.unc.edu>
2023-05-08 22:28:55 -0400

    server.js fixed

--- server.js
+++ server.js
@@ -71,6 +71,38 @@ app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:htt
 // Serve static files
 const staticpath = args.stat || args.s || process.env.STATICPATH || path.join(__dirname, 'public')
 app.use('/', express.static(staticpath))
+
+//A04 CODE BEGIN
+
+import {rps, rpsls} from "./lib/rpsls.js";
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get("/app", (req, res) => {
+    res.status(200).send("200 OK");});
+app.get("/app/rps", (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));});
+app.get("/app/rpsls", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));});
+app.get("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));});
+app.get("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));});
+app.post("/app/rps/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));});
+app.post("/app/rpsls/play", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));});
+app.get("/app/rps/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));});
+app.get("/app/rpsls/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));});
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");});
+app.listen(PORT, () => {
+    console.log(`the app is lisning on port ${PORT}`);});
+
+//A04 CODE END
+
 // Create app listener
 const server = app.listen(port)
 // Create a log entry on start
